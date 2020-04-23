ac5db1b935c135fd9670c27825e738d447e1d2a5
Lars Kliesing <lgk@live.de>
2020-04-23 15:57:14 +0200

    questions over questions

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { Fragment } from "react";
 
-import { IItem, IPlayer } from "./shared/types";
+import { IItem, IPlayer, IItemCoordinated, IRow, ColumnArray, IColumn } from "./shared/types";
 import { pad } from "./shared/helpers";
 import { TimeLeft } from "./components/TimeLeft";
 import { DEBUG } from "./shared/constants";
@@ -52,10 +52,11 @@ const maxLamas = 10;
 export interface IAppProps { }
 export interface IAppState {
     activeItems: IItem[];
+    rows: IRow[];
     players: IPlayer[];
     nextItems: IItem[];
     currentPlayerIndex: 0 | 1;
-    currentItem: IItem;
+    currentItem: IItem | null;
 }
 
 export class App extends React.Component<IAppProps, IAppState> {
@@ -75,6 +76,13 @@ export class App extends React.Component<IAppProps, IAppState> {
             Object.assign({ x: 2, y: 4 }, data.items.pear),
             Object.assign({ x: 3, y: 4 }, data.items.banana),
             Object.assign({ x: 4, y: 4 }, data.items.bell),
+        ],
+        rows: [
+            { columns: [null, data.items.cherry, null, null, null] as ColumnArray },
+            { columns: [null, data.items.bell, null, null, null] as ColumnArray },
+            { columns: [null, data.items.pear, data.items.pear, null, data.items.bar] as ColumnArray },
+            { columns: [data.items.pear, data.items.cherry, data.items.bell, null, data.items.bell] as ColumnArray },
+            { columns: [data.items.banana, data.items.bell, data.items.pear, data.items.banana, data.items.bell] as ColumnArray },
         ],
         players: [
             {
@@ -124,7 +132,18 @@ export class App extends React.Component<IAppProps, IAppState> {
         // this.makeMove();
     }
 
-    makeMove() {
+    getItemByCoordinates(x: number, y: number, items: IItem[] = this.state.activeItems): IItem | null {
+        let i = 0;
+        for (i; i < items.length; i++) {
+            const item = items[i];
+            if (item.x === x && item.y === y) {
+                return item;
+            }
+        }
+        return null;
+    }
+
+    makeMove_before() {
         let { currentItem, activeItems } = this.state;
 
         if (currentItem.x === -1) {
@@ -133,29 +152,146 @@ export class App extends React.Component<IAppProps, IAppState> {
 
         if (currentItem.x === 5) {
             // Slide from right
-            activeItems.forEach((item: IItem) => {
-                if (item.y === currentItem.y) {
-                    // Is in this line
 
-                    if (item.x === 4) {
+            let keepSlideing = true;
+
+            const thisLine = activeItems.filter((item: IItemCoordinated) => item.y === currentItem.y)
+                .sort((a: IItemCoordinated, b: IItemCoordinated) => a.x > b.x ? -1 : 1);
+            console.log(thisLine);
+
+            // thisLine.forEach((item: IItemCoordinated, i: number) => {
+            //     if (keepSlideing) {
+            //         const next = thisLine[i + 1];
+            //         keepSlideing = (next && next.x === item.x - 1);
+
+            //         item.x = item.x - 1;
+            //     }
+            //     activeItems.push(item);
+            // });
+
+            activeItems.forEach((item: IItemCoordinated) => {
+                if (thisLine.includes(item)) {
+                    if (keepSlideing) {
                         item.x = item.x - 1;
                     }
                 }
             });
 
+            // activeItems.forEach((item: IItem) => {
+            //     if (item.y === currentItem.y) {
+            //         // Is in this line
+
+            //         if (keepSlideing && item.x !== undefined) {
+            //             const neighbourItem = this.getItemByCoordinates(item.x - 1, item.y, activeItems);
+            //             if (!neighbourItem) {
+            //                 keepSlideing = false;
+            //             }
+
+            //             item.x = item.x - 1;
+            //         }
+
+            //     }
+            // });
+
             const nextItem = this.getNextItem(currentItem.x, currentItem.y);
 
-            currentItem.x = 4;
-            activeItems.push(currentItem);
+            // currentItem.x = 4;
+            // activeItems.push(currentItem);
             this.setState({
                 currentItem: nextItem,
-                activeItems: activeItems
+                activeItems
             });
         }
 
         if (currentItem.y === -1) {
             // Slide from top
         }
+    }
+
+    // makeMove_after() {
+    //     let { rows, currentItem } = this.state;
+
+    //     console.log("make move");
+
+    //     const direction: "left" | "right" | "top" = (currentItem.x === -1) ? "left" : (currentItem.x === 5) ? "right" : "top";
+
+    //     if (direction === "left" || direction === "right") {
+    //         let thisColumns = rows[currentItem.y].columns;
+
+    //         if (direction === "right") {
+    //             thisColumns.reverse();
+    //         }
+
+    //         let keepSliding = true;
+
+    //         thisColumns.forEach((item: IItem | null, index: number) => {
+    //             const negIndex = thisColumns.length - index - 1;
+    //             const x = direction === "right" ? negIndex : index;
+    //             console.log(item, x);
+
+    //             if (keepSliding) {
+    //                 keepSliding = (rows[currentItem.y].columns[x - 1] !== null);
+    //                 rows[currentItem.y].columns[x] = null;
+    //                 rows[currentItem.y].columns[x - 1] = item;
+    //             }
+    //         });
+    //     }
+
+    //     this.setState({ rows });
+    // }
+
+    makeMove() {
+        let { currentItem, activeItems } = this.state;
+
+        const direction: "left" | "right" | "top" = (currentItem.x === -1) ? "left" : (currentItem.x === 5) ? "right" : "top";
+
+        // turn activeItems to IRow[]
+        let rows: IRow[] = new Array<IRow>(5);
+
+        activeItems.forEach((item: IItemCoordinated, index: number) => {
+            if (!rows[item.y]) rows[item.y] = { columns: [null, null, null, null] as unknown as ColumnArray };
+            rows[item.y].columns[item.x] = Object.assign({ itemIndex: index }, item);
+        });
+
+        console.log(rows);
+
+        let keepSliding = true;
+
+        const line = direction === "right" ? rows[currentItem.y].columns.reverse() : rows[currentItem.y].columns;
+
+        line.forEach((column: IColumn | null, index: number) => {
+            if (keepSliding && column) {
+                keepSliding = (rows[currentItem.y].columns[index + 1] !== null);
+
+                if (direction === "left") {
+                    activeItems[column.itemIndex].x++;
+                }
+                else if (direction === "right") {
+                    activeItems[column.itemIndex].x--;
+                }
+            }
+
+            if (!column) keepSliding = false;
+        });
+
+        if (direction === "left") {
+            currentItem.x = 0;
+        }
+        if (direction === "right") {
+            currentItem.x = 4;
+        }
+        activeItems.push(currentItem);
+
+        const nextItem = this.getNextItem(currentItem.x, currentItem.y);
+
+        this.setState({
+            currentItem: null,
+            activeItems
+        }, () => setTimeout(() => {
+            this.setState({
+                currentItem: nextItem
+            });
+        }, 500));
     }
 
     render() {
@@ -214,18 +350,31 @@ export class App extends React.Component<IAppProps, IAppState> {
                                 </div>
                             )}
 
-                            <div
-                                className={`Item Grid-currentItem Grid-item Grid-item-${currentItem.x}-${currentItem.y} Grid-x-${currentItem.x} Grid-y-${currentItem.y}`}
-                            >
-                                {currentItem.icon}
-                            </div>
+                            {/* {this.state.rows.map((row: IRow, y: number) =>
+                                row.columns.map((item: IItem | null, x: number) =>
+                                    item &&
+                                    <div key={`${x}-${y}`} className={`Item Grid-item Grid-item-${x}-${y} ${item.new === true ? "new" : ""}`}>
+                                        {item.icon}
+                                    </div>
+                                )
+                            )} */}
+
+                            {currentItem &&
+                                <div
+                                    className={`Item Grid-currentItem Grid-item Grid-item-${currentItem.x}-${currentItem.y} Grid-x-${currentItem.x} Grid-y-${currentItem.y}`}
+                                >
+                                    {currentItem.icon}
+                                </div>
+                            }
 
                             <GridOverlay
                                 onHover={(x: number, y: number) => {
                                     let currentItem = this.state.currentItem;
-                                    currentItem.x = x;
-                                    currentItem.y = y;
-                                    this.setState({ currentItem });
+                                    if (currentItem) {
+                                        currentItem.x = x;
+                                        currentItem.y = y;
+                                        this.setState({ currentItem });
+                                    }
                                 }}
                             />
                         </div>
--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,3 +1,8 @@
+type Grow<T, A extends Array<T>> = ((x: T, ...xs: A) => void) extends ((...a: infer X) => void) ? X : never;
+type GrowToSize<T, A extends Array<T>, N extends number> = { 0: A, 1: GrowToSize<T, Grow<T, A>, N> }[A['length'] extends N ? 0 : 1];
+
+export type FixedArray<T, N extends number> = GrowToSize<T, [], N>;
+
 export interface IItem {
     name: string;
     icon: string;
@@ -7,9 +12,24 @@ export interface IItem {
     new?: boolean;
 }
 
+export interface IItemCoordinated extends IItem {
+    x: number;
+    y: number;
+}
+
 export interface IPlayer {
     name: string;
     points: number;
     lamas: number;
     color: "red" | "blue";
+}
+
+export interface IColumn extends IItem {
+    itemIndex: number;
+}
+
+export type ColumnArray = FixedArray<IColumn | null, 5>;
+
+export interface IRow {
+    columns: ColumnArray;
 }
